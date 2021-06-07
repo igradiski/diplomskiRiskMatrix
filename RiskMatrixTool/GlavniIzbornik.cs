c2af8dde40f85aa8366364d81b413ab0938acc6c
DESKTOP-3MKTUTH\Ivor <igradiski@foi.hr>
2021-06-07 17:19:56 +0200

    Kreiran projekt

---
+++ RiskMatrixTool/GlavniIzbornik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RiskMatrixTool
+{
+    public partial class GlavniIzbornik : Form
+    {
+        public GlavniIzbornik()
+        {
+            InitializeComponent();
+        }
+
+        private void GlavniIzbornik_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
